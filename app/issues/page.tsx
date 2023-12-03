96ff15116f49e58cdb2d89ae66ada2fa19882ee8
saif <saif.s@bsfdv.com>
2023-12-03 19:37:04 +0400

    feat: setup radix ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@radix-ui/themes";
 import React from "react";
 
 const IssuesPage = () => {
-  return <div>IssuesPage</div>;
+  return (
+    <div>
+      IssuesPage
+      <Button>New Issue</Button>
+    </div>
+  );
 };
 
 export default IssuesPage;
