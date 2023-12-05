47e5121bfc4c837676e007b9c8bbca3269646c5c
saif <saif.s@bsfdv.com>
2023-12-05 12:14:33 +0400

    feat: updating issue api

---
+++ app/api/issues/[id]/route.ts
@@ -0,0 +1,26 @@
+import { issueSchema } from "@/app/validationSchemas";
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();
+  const validation = issueSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400 });
+
+  const issue = prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!issue)
+    return NextResponse.json({ error: "Invalid Issue" }, { status: 404 });
+
+  const updatedIssue = await prisma.issue.update({
+    where: { id: parseInt(params.id) },
+    data: { title: body.tile, description: body.description },
+  });
+
+  return NextResponse.json(updatedIssue);
+}
